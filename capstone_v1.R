b0166657d0239aa5368cfc2d55fe302c9d89de54
Lee Lou <lee.lou@novobi.com>
2018-11-17 15:38:17 -0800

    Duy commit code

--- capstone_v1.R
+++ capstone_v1.R
@@ -3,7 +3,7 @@
 #install.packages("data.table")
 library(data.table)
 train.data <- fread("param_data_train.csv", header = TRUE)
-
+#ABCXYZ123
 ###########################################################################################
 ###################################Data Modification#######################################
 
